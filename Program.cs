a8f3ca27ff97f5bab4da9af4a29449ae235dea70
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-20 21:59:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AnaliseCredito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor solicitado do empréstimo: ");
+            double emprestimo=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite a quantidade de parcelas desejada: ");
+            double parcela=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite a sua renda mensal: ");
+            double renda=Convert.ToDouble(Console.ReadLine());
+
+            double resultado=(30 * renda) / 100;
+            
+            Console.Clear();
+
+            if(resultado>parcela)
+            Console.WriteLine("Empréstimo negado");
+            else
+            Console.WriteLine("Empréstimo aprovado");
+
+        
+
+
+
+
+        }
+    }
+}
